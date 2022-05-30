cb8bb1444a6900371a5b5e637a95a603051fd054
Irving gonçalves teixeira da silva <106492630+irvinggtei@users.noreply.github.com>
2022-05-30 16:55:09 -0300

    Add files via upload

---
+++ todolist.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+
+def janela_inicial():
+    sg.theme('DarkBlue4')
+    linha = [
+        [sg.Checkbox(''), sg.Input('')]
+    ]
+    layout = [
+        [sg.Frame('Tarefas', layout=linha, key='container')],
+        [sg.Button('Nova Tarefa'), sg.Button('Resetar')]
+    ]
+    return sg.Window('Tarefas', layout=layout, finalize=True)
+
+janela = janela_inicial()
+
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == 'Nova Tarefa':
+        janela.extend_layout(janela['container'], [[sg.Checkbox (''), sg.Input('')]])
+    elif event == 'Resetar':
+        janela.close()
+        janela = janela_inicial()
